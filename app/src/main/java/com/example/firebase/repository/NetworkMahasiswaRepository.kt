6802412562ddabbc79fc47fea39b96b2c243efff
lenovo <nico.dwi.ft22@mail.umy.c.id>
2025-01-13 10:36:19 +0700

    membuat InsertMahasiswa

--- app/src/main/java/com/example/firebase/repository/NetworkMahasiswaRepository.kt
+++ app/src/main/java/com/example/firebase/repository/NetworkMahasiswaRepository.kt
@@ -6,44 +6,46 @@ import com.google.firebase.firestore.Query
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
-
-class NetworkMahasiswaRepository(
-    private val firestore: FirebaseFirestore
-):MahasiswaRepository {
-    override suspend fun getAllMahasiswa(): Flow<List<Mahasiswa>> = callbackFlow{
-
+import kotlinx.coroutines.tasks.await
+class NetworkMahasiswaRepository(private val firestore: FirebaseFirestore) : MahasiswaRepository {
+    override suspend fun getAllMahasiswa(): Flow<List<Mahasiswa>> = callbackFlow {
         val mhsCollection = firestore.collection("Mahasiswa")
             .orderBy("nim", Query.Direction.DESCENDING)
             .addSnapshotListener { value, error ->
-
                 if (value != null) {
                     val mhsList = value.documents.mapNotNull {
                         it.toObject(Mahasiswa::class.java)!!
                     }
                     trySend(mhsList)
                 }
-
             }
         awaitClose {
             mhsCollection.remove()
         }
-        TODO("Not yet implemented")
     }
 
     override suspend fun insertMahasiswa(mahasiswa: Mahasiswa) {
-        TODO("Not yet implemented")
+        try {
+            firestore.collection("Mahasiswa")
+                .document(mahasiswa.nim) // Gunakan nim sebagai ID dokumen
+                .set(mahasiswa)
+                .await()
+        } catch (e: Exception) {
+            throw Exception("Gagal menambahkan data mahasiswa: ${e.message}")
+        }
     }
 
+
     override suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa) {
-        TODO("Not yet implemented")
+
     }
 
     override suspend fun deleteMahasiswa(nim: String) {
-        TODO("Not yet implemented")
+
     }
 
-    override suspend fun getMahasiswaByNim(nim: String): Flow<Mahasiswa> {
-        TODO("Not yet implemented")
-    }
 
-}
+    override suspend fun getMahasiswaByNim(nim: String): Flow<Mahasiswa> = callbackFlow {
+
+        }
+    }
