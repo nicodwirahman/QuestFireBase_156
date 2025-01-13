15afe2912254b56a49613cd65d2ed58b1c3f7d3e
lenovo <nico.dwi.ft22@mail.umy.c.id>
2025-01-13 11:00:09 +0700

    membuat sealed Class

--- app/src/main/java/com/example/firebase/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/firebase/ui/viewmodel/InsertViewModel.kt
@@ -4,6 +4,12 @@ import com.example.firebase.model.Mahasiswa
 
 
 
+sealed class FormState {
+    object Idle : FormState()
+    object Loading : FormState()
+    data class Success(val message: String) : FormState()
+    data class Error(val message: String) : FormState()
+}
 
 data class InsertUiState(
     val insertUiEvent: MahasiswaEvent = MahasiswaEvent(),
