9f65ef7281c21449ec20af21706b7a7b0691b318
lenovo <nico.dwi.ft22@mail.umy.c.id>
2025-01-13 10:57:45 +0700

    membuat data class formErrorState

--- app/src/main/java/com/example/firebase/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/firebase/ui/viewmodel/InsertViewModel.kt
@@ -3,6 +3,20 @@ package com.example.firebase.ui.viewmodel
 import com.example.firebase.model.Mahasiswa
 
 
+data class FormErrorState(
+    val nim: String? = null,
+    val nama: String? = null,
+    val jenisKelamin: String? = null,
+    val alamat: String? = null,
+    val kelas: String? = null,
+    val angkatan: String? = null
+) {
+    fun isValid(): Boolean {
+        return nim == null && nama == null && jenisKelamin == null &&
+                alamat == null && kelas == null && angkatan == null
+    }
+}
+
 data class MahasiswaEvent(
     val nim: String = "",
     val nama: String = "",
